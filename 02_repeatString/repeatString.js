08f4ab7b4a72f8860b2c1f1ba4524d8b3a21f311
Maria Lujan Sosa Balsa <luubalsa@gmail.com>
2022-07-13 18:14:10 +0200

    ejercicios odin js 1-3 completados

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let message = "";
+    for (let i = 1; i <= num; i++){
+       message += string;
+    }
+    if (num < 0) {
+        return "ERROR";
+    }
+    else if (num === 0) {
+        return "";
+    }
+    else {
+        return message; 
+    }
+    
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let reversedArray = Array.from(string);
+let reversedString = reversedArray.reverse();
+reversedString.toString();
+return reversedString.join("");
 };
 
 // Do not edit below this line
