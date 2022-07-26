5c2a3415598c0e8834f7a178c6a20438ec8065ef
Maria Lujan Sosa Balsa <luubalsa@gmail.com>
2022-07-26 16:30:57 +0200

    ejercicio terminado

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(theArray) {
+let oldest = 0;
+let age;
+let oldestPerson;
+let currentYear = new Date().getFullYear();
+for (let i = 0; i < theArray.length; i++) {
+    if (theArray[i].yearOfDeath) {
+        age = theArray[i].yearOfDeath - theArray[i].yearOfBirth;
+        theArray[i].Age = age;
+    }
+    else {
+        age = currentYear - theArray[i].yearOfBirth;
+        theArray[i].Age = age;
+    }
+    if (age > oldest) {
+        oldest = age;
+        oldestPerson = theArray[i];
+    }
+};
+return oldestPerson;
 };
 
 // Do not edit below this line
