4c739b169dcb778f7837198036f39c2372333ba5
Maria Lujan Sosa Balsa <luubalsa@gmail.com>
2022-07-26 12:52:51 +0200

    ejercicio terminado

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return (a + b);
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(sumArray) {
+  let result = 0;
+  for (let i = 0; i < sumArray.length; i++) {
+    result += sumArray[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(multiplyArray) {
+ let result = 1;
+ for (let i = 0; i < multiplyArray.length; i++) {
+  result *= multiplyArray[i];
+ };
+ return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+let result = 1;
+for (let i = 0; i < b; i++) {
+  result *= a;
+};
+return result;
 };
 
-const factorial = function() {
-	
+//factorial is the multiplication of all integers up to a number
+const factorial = function(num) {
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  };
+	return result;
 };
 
 // Do not edit below this line
