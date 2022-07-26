35b2f9a197b179a3939b8b584a7ecf6132d3bdf2
Maria Lujan Sosa Balsa <luubalsa@gmail.com>
2022-07-26 15:38:08 +0200

    ejercicio completo

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(booksArray) {
+    let titles = [];
+    for (let i = 0; i < booksArray.length; i++) {
+        titles.push(booksArray[i].title)
+    }
+    return titles;
 };
 
 // Do not edit below this line
