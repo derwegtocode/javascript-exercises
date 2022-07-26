33b095923d8432c21881a37d29731317394c7d6e
Maria Lujan Sosa Balsa <luubalsa@gmail.com>
2022-07-26 14:38:03 +0200

    ejercicio terminado

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+let fiboArray = [1, 1];
+
+if (num <= 0) {
+    return "OOPS";
+}
+else {
+   for (let i = 0; i < num; i++) {
+let fibo = fiboArray[i] + fiboArray[i+1];
+fiboArray.push(fibo);
+};
+return fiboArray[num-1]; 
+};
 
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
